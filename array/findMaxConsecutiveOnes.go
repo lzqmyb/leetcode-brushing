b0afe1d0d8e0e57da673444d5cef1453f4dbfebf
lzqmyb <lvzq@missfresh.cn>
2018-06-01 18:22:41 +0800

    update

---
+++ array/findMaxConsecutiveOnes.go
@@ -0,0 +1,51 @@
+package array
+
+func FindMaxConsecutiveOnes(nums []int) int {
+	var result  int
+
+	if len(nums) == 0 {
+		panic("nums lenth is zero")
+	}
+
+	length := 0
+	for _, v := range nums {
+		if v == 1 {
+			length++
+			if result < length {
+				result = length
+			}
+		} else {
+			length = 0
+		}
+	}
+
+	return result
+}
+
+func FindMaxConsecutiveOnesBetter(nums []int) int {
+	if len(nums) == 0 {
+		panic("error")
+	}
+	maxCount := 0
+	count := 0
+	length := len(nums)
+
+	for i := 0; i< length; i++{
+		if nums[i] == 1 {
+			count++
+		} else {
+			if maxCount < count {
+				maxCount = count
+			}
+
+			count = 0
+		}
+	}
+
+	if maxCount < count {
+		maxCount = count
+	}
+
+	return maxCount
+
+}
--- main.go
+++ main.go
@@ -6,13 +6,15 @@ import (
 )
 
 func main()  {
-	l1 := array.GenerateListNode([]int{1})
-	l2 := array.GenerateListNode([]int{9,9})
-	result := array.AddTwoNumbers(l1,l2)
-	for result != nil {
-		fmt.Println(result.Val)
-		result = result.Next
-	}
+	//l1 := array.GenerateListNode([]int{1})
+	//l2 := array.GenerateListNode([]int{9,9})
+	//result := array.AddTwoNumbers(l1,l2)
+	//for result != nil {
+	//	fmt.Println(result.Val)
+	//	result = result.Next
+	//}
+	result := array.SingleNumber([]int{2,2,1})
+	result = array.SingleNumberBetter([]int{4,1,2,1,2})
+	fmt.Println(result)
 }
 
-//[]
